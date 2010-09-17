e2096112ce1f3a436704c56fc98814d335826905
Jacek Bzdak <jbzdak@gmail.com>
2010-09-17 15:18:46 +0200

    Initial version

---
+++ src/main/java/cx/ath/jbzdak/common/PropertyHolder.java
@@ -0,0 +1,37 @@
+package cx.ath.jbzdak.common;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Jacek Bzdak
+ */
+public class PropertyHolder {
+
+   protected Properties properties;
+
+   public PropertyHolder(Class clazz, String defaultResources, String userResources) throws IOException {
+      properties = new Properties();
+      try {
+         properties.load(clazz.getResourceAsStream(defaultResources));
+      } catch (IOException e) {
+         throw new IOException("Couldnt load default properties", e);
+      } catch (NullPointerException e){
+         throw new IOException("Couldnt load default properties", e);
+      }
+      File file = new File(userResources);
+      file.getParentFile().mkdirs();
+      if (!file.exists()){
+         file.createNewFile();
+      }
+      if (file.exists()){
+         properties = new Properties(properties);
+         properties.load(new FileInputStream(file));
+      }else{
+         properties.store(new FileOutputStream(file), "Auto generated");
+      }
+   }
+}
