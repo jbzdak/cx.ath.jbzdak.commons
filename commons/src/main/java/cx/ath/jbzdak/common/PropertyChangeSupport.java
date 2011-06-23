0d837363830ed3815ab5178592a9db4da45e2fd4
Jacek Bzdak <jbzdak@gmail.com>
2011-06-23 12:16:02 +0200

    Updated copyright

--- commons/src/main/java/cx/ath/jbzdak/common/PropertyChangeSupport.java
+++ commons/src/main/java/cx/ath/jbzdak/common/PropertyChangeSupport.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (c) 2009. by Jacek Bzdak
+ * Copyright for Jacek Bzdak 2011.
  *
- *     This file is part of Jacek Bzdak commons library
+ * This file is part of my commons library.
  *
- *     This library is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     This library is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ * It is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 
 package cx.ath.jbzdak.common;
--- commons/src/main/java/cx/ath/jbzdak/common/ProxyPropertyChangeListener.java
+++ commons/src/main/java/cx/ath/jbzdak/common/ProxyPropertyChangeListener.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright for Jacek Bzdak 2011.
+ *
+ * This file is part of my commons library.
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * It is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package cx.ath.jbzdak.common;
 
 import java.beans.PropertyChangeEvent;
